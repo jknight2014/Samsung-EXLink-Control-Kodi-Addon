6330d592b5a3d18d702b33bbdf774a8a59b364f3
jknight2014 <jpaytoncfd@gmail.com>
2016-01-04 22:47:22 -0500

    Simplified the script for testing.
    This is currently working however its limited to /dev/ttyUSB0 (Unless
    you edit the addon.py file) Next version to include arguments for
    Dedicated On and Off.

--- addon.py
+++ addon.py
@@ -2,84 +2,30 @@
 import xbmcgui
 import serial
 import time
+import urlparse
 
 addon       = xbmcaddon.Addon()
 addonname   = addon.getAddonInfo('name')
 
-base_url = sys.argv[0]
-addon_handle = int(sys.argv[1])
-args = urlparse.parse_qs(sys.argv[2][1:])
+ser = serial.Serial(port='/dev/ttyS0', baudrate=9600, timeout=1)
+resp=""
+loop="true"
+string="030cf100"
+count=0
+while loop:
+        if count == 10:
+                break
+                print "TV Did Not Respond Success"
+        if string in resp:
+                print "found it"
+                break
+                ser.close()
+        else:
+                ser.write("\x08\x22\x00\x00\x00\x00\xd6")
+                data = ser.read(24)
+                resp=data.encode('hex')
+                print resp
+                count = count +1
+                time.sleep(1)
+        print "finished"
 
-def build_url(query):
-    return base_url + '?' + urllib.urlencode(query)
-
-mode = args.get('mode', None)
-
-if mode is None:
-	ser = serial.Serial(port='/dev/ttyUSB0', baudrate=9600, timeout=1)
-	resp=""
-	loop="true"
-	string="030cf100"
-	count=0
-	while loop:
-			if count == 10:
-					break
-					print "TV Did Not Respond Success"
-			if string in resp:
-					print "found it"
-					break
-					ser.close()
-			else:
-					ser.write("\x08\x22\x00\x00\x00\x00\xd6")
-					data = ser.read(24)
-					resp=data.encode('hex')
-					print resp
-					count = count +1
-					time.sleep(1)
-	print "finished"
-
-elif mode[0] == 'on':
-	ser = serial.Serial(port='/dev/ttyUSB0', baudrate=9600, timeout=1)
-	resp=""
-	loop="true"
-	string="030cf100"
-	count=0
-	while loop:
-			if count == 10:
-					break
-					print "TV Did Not Respond Success"
-			if string in resp:
-					print "found it"
-					break
-					ser.close()
-			else:
-					ser.write("\x08\x22\x00\x00\x00\x01\xd5")
-					data = ser.read(24)
-					resp=data.encode('hex')
-					print resp
-					count = count +1
-					time.sleep(1)
-	print "finished"
-
-elif mode[0] == 'off':
-	ser = serial.Serial(port='/dev/ttyUSB0', baudrate=9600, timeout=1)
-	resp=""
-	loop="true"
-	string="030cf100"
-	count=0
-	while loop:
-			if count == 10:
-					break
-					print "TV Did Not Respond Success"
-			if string in resp:
-					print "found it"
-					break
-					ser.close()
-			else:
-					ser.write("\x08\x22\x00\x00\x00\x02\xd4")
-					data = ser.read(24)
-					resp=data.encode('hex')
-					print resp
-					count = count +1
-					time.sleep(1)
-	print "finished"
